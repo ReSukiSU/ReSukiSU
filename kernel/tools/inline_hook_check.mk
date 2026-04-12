6763639082ba9ad28f354f67a951774526e959b0
luyanci <luyancib@qq.com>
2026-04-12 22:58:03 +0800

    kernel: add CONFIG_KSU_MANUAL_HOOK guard check for SUSFS Inline Hook Some kernel dev/build will make some SUSFS inline hook guarded by CONFIG_KSU_MANUAL_HOOK. but Kconfig will turn off CONFIG_KSU_MANUAL_HOOK when CONFIG_KSU_SUSFS enabled,and trigger the unexcepted bug(It's not our fault). So added the warning,hoping that it works.

--- kernel/tools/inline_hook_check.mk
+++ kernel/tools/inline_hook_check.mk
@@ -16,11 +16,24 @@ define check_ksu_hook_incompatible
     endif
 endef
 
+define check_ksu_manual_guard
+    ifeq ($$(shell grep -wq "CONFIG_KSU_MANUAL_HOOK" $(1); echo $$$$?),0)
+        $$(warning Detected KSU_MANUAL_HOOK guard in $(1) file, your build maybe broken. If $(2) happen, please check your hook and feedback to your SuSFS Patches Author.)
+    endif
+endef
+
 $(eval $(call check_ksu_hook_incompatible,ksu_vfs_read_hook,$(srctree)/fs/read_write.c))
 
+$(eval $(call check_ksu_manual_guard,$(srctree)/kernel/sys.c,cannot recognize manager))
 $(eval $(call check_ksu_hook,ksu_handle_setresuid,$(srctree)/kernel/sys.c))
+$(eval $(call check_ksu_manual_guard,$(srctree)/fs/exec.c,failed to grant to root or module not working))
 $(eval $(call check_ksu_hook,ksu_handle_execveat,$(srctree)/fs/exec.c))
+$(eval $(call check_ksu_manual_guard,$(srctree)/fs/open.c,failed to grant to root))
 $(eval $(call check_ksu_hook,ksu_handle_faccessat,$(srctree)/fs/open.c))
+$(eval $(call check_ksu_manual_guard,$(srctree)/fs/stat.c,cannot execute su))
 $(eval $(call check_ksu_hook,ksu_handle_stat,$(srctree)/fs/stat.c))
+$(eval $(call check_ksu_manual_guard,$(srctree)/kernel/reboot.c,module not working))
 $(eval $(call check_ksu_hook,ksu_handle_sys_reboot,$(srctree)/kernel/reboot.c))
+$(eval $(call check_ksu_manual_guard,$(srctree)/drivers/input/input.c,cannot trigger safemode by press volume down button three times and more))
 $(eval $(call check_ksu_hook,ksu_handle_input_handle_event,$(srctree)/drivers/input/input.c))
+
