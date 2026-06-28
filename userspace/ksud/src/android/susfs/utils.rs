bbfacb00d8a16597e2617441cc69f6c38c8a4f55
喵喵机 <2438926613@qq.com>
2026-06-28 10:49:15 +0800

    ksud-susfs: rm OnceLock in utils

--- userspace/ksud/src/android/susfs/utils.rs
+++ userspace/ksud/src/android/susfs/utils.rs
@@ -20,23 +20,15 @@ pub fn c_array_to_string<const N: usize>(array: &[u8; N]) -> String {
 }
 
 pub fn is_valid_uname_version(version: &str) -> bool {
-    static RE: OnceLock<Regex> = OnceLock::new();
-
-    let regex = RE.get_or_init(|| {
-        Regex::new(r"^([0-9]+)\.([0-9]+)(?:\.([0-9]+))?(?:-([a-zA-Z0-9.-]+))?$").unwrap()
-    });
+    let regex = Regex::new(r"^([0-9]+)\.([0-9]+)(?:\.([0-9]+))?(?:-([a-zA-Z0-9.-]+))?$").unwrap();
 
     regex.is_match(version)
 }
 
 pub fn is_valid_uname_release(release: &str) -> bool {
-    static RE: OnceLock<Regex> = OnceLock::new();
-
-    let regex = RE.get_or_init(|| {
-        Regex::new(
-            r"^#[0-9]+(?:\s+[A-Z0-9_]+)*\s+(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s+(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+[1-3]?[0-9]\s+[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\s+[A-Z]{3,4}\s+[0-9]{4}\s+[a-zA-Z0-9_.-]+\s+Android$"
-        ).unwrap()
-    });
+    let regex = Regex::new(
+        r"^#[0-9]+(?:\s+[A-Z0-9_]+)*\s+(?:Mon|Tue|Wed|Thu|Fri|Sat|Sun)\s+(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s+[1-3]?[0-9]\s+[0-2][0-9]:[0-5][0-9]:[0-5][0-9]\s+[A-Z]{3,4}\s+[0-9]{4}\s+[a-zA-Z0-9_.-]+\s+Android$"
+    ).unwrap();
 
     regex.is_match(release)
 }
