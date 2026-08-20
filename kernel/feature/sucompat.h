KernelSU-Next/KernelSU-Next

kernel, ksud: refactor sucompat (https://github.com/tiann/KernelSU/pull/3521)
- Execute ksud by opening fd with ksu cred and redirecting to execveat,
remove READ_SEARCH capability from effective caps.
- Redirect newfstatat/faccessat's `su` path to `ksud` .
- Now sucompat requires `/data/adb/ksud` to exist in caller's root, so
chrooted process won't be affected unexpectedly.
- Remove fallback to sh. User should install manager and open it after
first installation to install ksud to enable sucompat after first
installation.

kernel/feature/sucompat.h
@@ -9,9 +9,8 @@ void ksu_sucompat_init(void);
 void ksu_sucompat_exit(void);
 
 // Handler functions exported for hook_manager
-int ksu_handle_faccessat(int *dfd, const char __user **filename_user, int *mode,
-                         int *__unused_flags);
-int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
-long ksu_handle_execve_sucompat(const char __user **filename_user, int orig_nr, const struct pt_regs *regs);
+long ksu_handle_faccessat_sucompat(int orig_nr, struct pt_regs *regs);
+long ksu_handle_stat_sucompat(int orig_nr, struct pt_regs *regs);
+long ksu_handle_execve_sucompat(const char __user **filename_user, int orig_nr, struct pt_regs *regs);
 
 #endif

kernel/hook/syscall_event_bridge.c
@@ -49,36 +49,18 @@ static int ksu_handle_init_mark_tracker(const char __user **filename_user)
 
 long __nocfi ksu_hook_newfstatat(int orig_nr, const struct pt_regs *regs)
 {
-    int *dfd;
-    const char __user **filename_user;
-    int *flags;
-
     if (!ksu_su_compat_enabled)
         return ksu_syscall_table[orig_nr](regs);
 
-    dfd = (int *)&PT_REGS_PARM1(regs);
-    filename_user = (const char __user **)&PT_REGS_PARM2(regs);
-    flags = (int *)&PT_REGS_SYSCALL_PARM4(regs);
-    ksu_handle_stat(dfd, filename_user, flags);
-
-    return ksu_syscall_table[orig_nr](regs);
+    return ksu_handle_stat_sucompat(orig_nr, (struct pt_regs *)regs);
 }
 
 long __nocfi ksu_hook_faccessat(int orig_nr, const struct pt_regs *regs)
 {
-    int *dfd;
-    const char __user **filename_user;
-    int *mode;
-
     if (!ksu_su_compat_enabled)
         return ksu_syscall_table[orig_nr](regs);
 
-    dfd = (int *)&PT_REGS_PARM1(regs);
-    filename_user = (const char __user **)&PT_REGS_PARM2(regs);
-    mode = (int *)&PT_REGS_PARM3(regs);
-    ksu_handle_faccessat(dfd, filename_user, mode, NULL);
-
-    return ksu_syscall_table[orig_nr](regs);
+    return ksu_handle_faccessat_sucompat(orig_nr, (struct pt_regs *)regs);
 }
 
 DEFINE_STATIC_KEY_TRUE(ksud_execve_key);
@@ -109,7 +91,7 @@ long __nocfi ksu_hook_execve(int orig_nr, const struct pt_regs *regs)
             pr_err("adb root failed: %ld\n", ret);
         }
     } else if (ksu_su_compat_enabled) {
-        ret = ksu_handle_execve_sucompat(filename_user, orig_nr, regs);
+        ret = ksu_handle_execve_sucompat(filename_user, orig_nr, (struct pt_regs *)regs);
         ksu_sulog_emit_pending(pending_root_execve, ret, GFP_KERNEL);
         return ret;
     }

ReSukiSU/ReSukiSU

Downstream changes:
Reapply READ_SEARCH capability for effective caps
Reason: Manual hook can't override_creds & execute orig & revert_creds
Redirect newfstatat/faccessat's 'su' path to 'sh' for manual hook
[cherry-picked from upstream commit https://github.com/tiann/KernelSU/commit/f0f4539b34cb28efdef2ed96ab2a1dfe112eaab8]
Signed-off-by: AlexLiuDev233 <wzylin11@outlook.com>

kernel/feature/sucompat.h
@@ -24,7 +24,9 @@ int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
 #ifdef CONFIG_KSU_TRACEPOINT_HOOK
 // WARNING! THERE HAVE TRYING TO CALL SYSCALL INTERNALLY
 // ENSURE CALL IT ONLY IN TRACEPOINT SYSCALL REDIRECT
-int ksu_handle_execve_sucompat_tp_internal(const char __user **filename_user, int orig_nr, const struct pt_regs *regs);
+long ksu_handle_faccessat_sucompat_internal(int orig_nr, struct pt_regs *regs);
+long ksu_handle_stat_sucompa_internal(int orig_nr, struct pt_regs *regs);
+long ksu_handle_execve_sucompat_internal(const char __user **filename_user, int orig_nr, struct pt_regs *regs);
 #else // #ifndef CONFIG_KSU_TRACEPOINT_HOOK
 
 // 63 already used as TIF_KSU_DISABLE_ESCAPE_WITH_ROOT (64bit)

kernel/hook/syscall_event_bridge.c
@@ -52,38 +52,18 @@ static int ksu_handle_init_mark_tracker(const char __user **filename_user)
 
 long __nocfi ksu_hook_newfstatat(int orig_nr, const struct pt_regs *regs)
 {
-    int *dfd;
-    const char __user **filename_user;
-    int *flags;
-
-    // GKI2 always have static_key
     if (!static_branch_unlikely(&ksu_su_compat_enabled))
         return ksu_syscall_table[orig_nr](regs);
 
-    dfd = (int *)&PT_REGS_PARM1(regs);
-    filename_user = (const char __user **)&PT_REGS_PARM2(regs);
-    flags = (int *)&PT_REGS_SYSCALL_PARM4(regs);
-    ksu_handle_stat(dfd, filename_user, flags);
-
-    return ksu_syscall_table[orig_nr](regs);
+    return ksu_handle_stat_sucompat_internal(orig_nr, (struct pt_regs *)regs);
 }
 
 long __nocfi ksu_hook_faccessat(int orig_nr, const struct pt_regs *regs)
 {
-    int *dfd;
-    const char __user **filename_user;
-    int *mode;
-
-    // GKI2 always have static_key
     if (!static_branch_unlikely(&ksu_su_compat_enabled))
         return ksu_syscall_table[orig_nr](regs);
 
-    dfd = (int *)&PT_REGS_PARM1(regs);
-    filename_user = (const char __user **)&PT_REGS_PARM2(regs);
-    mode = (int *)&PT_REGS_PARM3(regs);
-    ksu_handle_faccessat(dfd, filename_user, mode, NULL);
-
-    return ksu_syscall_table[orig_nr](regs);
+    return ksu_handle_faccessat_sucompat_internal(orig_nr, (struct pt_regs *)regs);
 }
 
 // there are for tracepoint syscall redirect hook
@@ -112,7 +92,7 @@ long __nocfi ksu_hook_execve(int orig_nr, const struct pt_regs *regs)
             pr_err("adb root failed: %ld\n", ret);
         }
     } else if (static_branch_likely(&ksu_su_compat_enabled)) {
-        ret = ksu_handle_execve_sucompat_tp_internal(filename_user, orig_nr, regs);
+        ret = ksu_handle_execve_sucompat_internal(filename_user, orig_nr, (struct pt_regs *)regs);
         ksu_sulog_emit_pending(pending_root_execve, ret, GFP_KERNEL);
         return ret;
     }